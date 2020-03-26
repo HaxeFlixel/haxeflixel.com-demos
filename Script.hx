2924a17e70378b8a8b4b2dc63c95c2805a4a66cf
Jens Fischer <jensfischer95@gmail.com>
2020-03-26 19:23:36 +0100

    Switch to GitHub actions

--- Script.hx
+++ Script.hx
@@ -1,23 +1,24 @@
-import haxe.io.Path;
 import sys.FileSystem;
-import sys.io.File;
-using StringTools;
 
-@:enum abstract Target(String) from String {
+enum abstract Target(String) from String {
 	var Flash = "flash";
 	var Html5 = "html5";
 }
 
 class Script {
 	static function main() {
+		FileSystem.createDirectory("bin");
+		Sys.setCwd("bin");
+
 		var args = Sys.args();
 		var command = args[0];
 		var target = args[1];
 
-		switch (command) {
-			case "build": build(target);
-			case "copy": copy(target, args[2]);
-			case _: Sys.println('unknown command $command');
+		switch command {
+			case "build":
+				build(target);
+			case _:
+				Sys.println('unknown command $command');
 		}
 	}
 
@@ -27,32 +28,6 @@ class Script {
 			args = args.concat(["--", "-dce", "full"]);
 		}
 		runCommand("haxelib", args);
-	}
-
-	static function copy(target:Target, flixelDemos:String) {
-		var dir = if (target == Flash) "swf" else "html5";
-		runCommand("rm", ["-rf", dir]);
-		runCommand("mkdir", [dir]);
-
-		for (category in FileSystem.readDirectory(flixelDemos)) {
-			var categoryFullPath = Path.join([flixelDemos, category]);
-			if (!FileSystem.isDirectory(categoryFullPath) || category.startsWith("."))
-				continue;
-
-			for (demo in FileSystem.readDirectory(categoryFullPath)) {
-				var demoFullPath = Path.join([categoryFullPath, demo]);
-				switch (target) {
-					case Flash:
-						var swfPath = Path.join([demoFullPath, 'export/flash/bin/$demo.swf']);
-						if (FileSystem.exists(swfPath))
-							File.copy(swfPath, '$dir/$demo.swf');
-					case Html5:
-						var html5Path = Path.join([demoFullPath, 'export/html5/bin']);
-						if (FileSystem.exists(html5Path))
-							runCommand("cp", ["-r", html5Path, '$dir/$demo']);
-				}
-			}
-		}
 	}
 
 	static function runCommand(cmd:String, args:Array<String>):Int {
